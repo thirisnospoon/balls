09ff7a05972efc6cf2176a64f50989297ab79ce9
Andrey <you@example.com>
2022-08-01 20:05:15 +0300

    random

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const ballButtonList = document.querySelectorAll('.static-ball');
 const arrow = document.querySelector('.arrow');
-
-const colorArray = ["76549A", "DF7861", "1C6DD0", "A3E4DB", "F6C6EA", "D9F8C4", "F37878", "A2B5BB", "7D9D9C", "85C88A"];
-
-ballButtonList.forEach((ballButton, index) => {
-    ballButton.addEventListener('click', event => {
-        arrow.style.animationDuration = `2.${index}s`;
-        arrow.style.fill = `#${colorArray[index]}`;
-        removeSelected();
-        ballButton.classList.add('selected');
-    });
-});
+const menu = document.querySelector('.menu');
+const addBtn = document.querySelector('.add-btn');
+const container = document.querySelector('.container');
 
 function removeSelected() {
     ballButtonList.forEach(ballButton => {
         ballButton.classList.remove('selected');
     });
 }
+
+addBtn.addEventListener('click', event => {
+    event.preventDefault();
+    let randomMovingBall = document.createElement('div'),
+        randomStaticBall = document.createElement('div');
+
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    let randomSpeed = Math.random();
+
+    randomMovingBall.style.backgroundColor = '#' + randomColor;
+    randomMovingBall.style.animationDuration = 2 + randomSpeed + "s";
+
+    randomStaticBall.style.backgroundColor = '#' + randomColor;
+    randomStaticBall.style.animationDuration = 2 + randomSpeed + "s";
+
+    randomMovingBall.classList.add('ball', 'anim-ball');
+    randomStaticBall.classList.add('ball', 'static-ball');
+
+    container.append(randomMovingBall);
+    menu.append(randomStaticBall);
+
+    let dynamicallyAddedBalls = document.querySelectorAll('.static-ball');
+
+    dynamicallyAddedBalls[dynamicallyAddedBalls.length - 1].addEventListener('click', e => {
+        arrow.style.fill = '#' + randomColor;
+    });
+});
+
+menu.addEventListener('click', event => {
+    if (event.target.classList.contains('ball')) {
+        let ballList = document.querySelectorAll('.static-ball');
+        for (item of ballList) {
+            item.classList.remove('selected');
+        }
+        event.target.classList.add('selected');
+    }
+});
